c8b4e363fc86205ac434f43d1169660db22d88ac
Aidil Kamal <aidilk.b20@mhs.unsyiah.ac.id>
2026-09-08 10:17:54 +0700

    meanmpilkan summary card beserta valuenya pada dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,12 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
+
 import Header from "@/components/Header";
+import SummaryCard from "@/components/SummaryCard";
+import salesData from "@/data/sales.json"
+import { SalesData } from "@/types";
+import { getTotalKunjungan, getRataRataEfektivitas, getTotalOrder, formatRupiah } from "@/lib/salesUtils";
 
 export default function DashboardPage() {
   const { user, isLoading } = useAuth();
@@ -19,10 +24,18 @@ export default function DashboardPage() {
     return <p>Loading...</p>;
   }
 
+  const data = salesData as SalesData[];
+
   return (
     <main>
       <Header />
-      {/* nanti SummaryCard, SalesTable, EffectivenessChart di sini */}
+      <div className="p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-6">
+          <SummaryCard label="Total kunjungan" value={`${getTotalKunjungan(data)}`}/>
+          <SummaryCard label="Rata-rata efektivitas" value={`${getRataRataEfektivitas(data)}%`}/>
+          <SummaryCard label="Total order" value={formatRupiah(getTotalOrder(data))}/>
+        </div>
+      </div>
     </main>
   );
 }
